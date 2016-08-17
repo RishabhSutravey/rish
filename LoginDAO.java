611171da95a9fbff79c87ead16ef02c7fe31fc1d
RishabhSutravey <rsutravey@gmail.com>
2016-08-17 16:43:51 +0530

    Add files via upload

---
+++ LoginDAO.java
@@ -0,0 +1,16 @@
+package com.servlet;
+
+public class LoginDAO {
+	public Boolean isValidUser(String userid,String pwd){
+		if(userid.equals("niit")&&pwd.equals("niit"))
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+		
+	}
+
+}
